42005384545db4f502edfa8d17044361c59a9f54
gwenaelgressier <97903290+gwenaelgressier@users.noreply.github.com>
2022-04-04 14:40:32 +0200

    recuperation de l'id des param et fetch le produit visee

--- js/product.js
+++ js/product.js
@@ -1,7 +1,9 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get("id");
+//
+const queryString = window.location.search; //sert a recup se qui a apres le ? dans l'url
+const urlParams = new URLSearchParams(queryString); //recup les param de l'url
+const id = urlParams.get("id"); //recupere l'id
 
+//mon fetch avec l'id recuperais de l'url
 fetch(`http://localhost:3000/api/products/${id}`)
   .then((response) => response.json())
   .then((res) => console.log(res));
