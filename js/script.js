a3a3d474aacb6e8d830749539c55ebf062436684
gwenaelgressier <97903290+gwenaelgressier@users.noreply.github.com>
2022-04-04 14:27:26 +0200

    insertion de toute la 1er vignette

--- js/script.js
+++ js/script.js
@@ -6,19 +6,23 @@ fetch("http://localhost:3000/api/products")
 function addProducts(data) {
   const imageUrl = data.imageUrl;
   const altTxt = data.altTxt;
+  const _id = data._id;
+  const name = data.name;
+  const description = data.description;
 
-  const anchor = makeanchor(); //cree l'element a
+  const anchor = makeanchor(_id); //cree l'element a
   const makearticle = document.createElement("article"); //cree l'element article
   const img = makeimg(imageUrl, altTxt); //cree l'element img
-  const h3 = makeh3(); //cree l'element img
-  const p = makep(); //cree l'element img
+  const h3 = makeh3(name); //cree l'element img
+  const p = makep(description); //cree l'element img
 
   appendChildanchor(anchor, makearticle); //appelle la fonction qui acroche anchor a l'id items
   appendChildtoarticle(makearticle, img, h3, p); //appelle la fonctin qui acroche img, h3, p a l'article
 }
 //creation de l'element a
-function makeanchor() {
+function makeanchor(_id) {
   const anchor = document.createElement("a"); //cree l'element a
+  anchor.href = "./product.html?id=" + _id; //met le href sur le a
   return anchor;
 }
 
@@ -31,16 +35,18 @@ function makeimg(imageUrl, altTxt) {
 }
 
 //creation de l'element h3
-function makeh3() {
+function makeh3(name) {
   const h3 = document.createElement("h3"); //cree l'element h3
   h3.classList.add("productName"); //ajoute comme nom de class productName
+  h3.textContent = name; //importe le textContent de h3
   return h3;
 }
 
 //creation de l'element p
-function makep() {
+function makep(description) {
   const p = document.createElement("p"); //cree l'element p
   p.classList.add("productDescription"); //ajoute comme nom de class productDescription
+  p.textContent = description; //importe le textContent de p
   return p;
 }
 
